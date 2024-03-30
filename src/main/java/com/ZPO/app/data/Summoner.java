8a4ac66673f77da5f6ff163f94a1475175049af7
unknown <boryckibartek@gmail.com>
2024-03-30 11:16:15 +0100

    Add code

---
+++ src/main/java/com/ZPO/app/data/Summoner.java
@@ -0,0 +1,25 @@
+package com.ZPO.app.data;
+
+public class Summoner {
+    private String summonerId;
+    private String tagLine;
+
+    public Summoner() {
+    }
+
+    public String getSummonerId() {
+        return summonerId;
+    }
+
+    public void setSummonerId(String summonerId) {
+        this.summonerId = summonerId;
+    }
+
+    public String getTagLine() {
+        return tagLine;
+    }
+
+    public void setTagLine(String tagLine) {
+        this.tagLine = tagLine;
+    }
+}
