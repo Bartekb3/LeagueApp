e185127b3f57c68321fc364b6f9df97729e18f30
Bartek Borycki <127530696+Bartekb3@users.noreply.github.com>
2024-04-06 22:38:07 +0200

    del

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.ZPO.app"},{"l":"com.ZPO.app.data"},{"l":"com.ZPO.app.exceptions"},{"l":"com.ZPO.app.views"},{"l":"com.ZPO.app.views.list"}];updateSearchResults();
